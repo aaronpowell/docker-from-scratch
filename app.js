ac95224ef1c9b303abdfb19b1e1d8015573d11fd
Aaron Powell <me@aaron-powell.com>
2017-07-26 22:04:30 +1000

    run nodejs app (no port binding)

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+}).listen(3000);
+
+console.log('server is up and running');
--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@ ECHO OFF
 
 ECHO nodejs image
 
-docker run -it --rm --name node node:7.7.4-alpine
+docker run -it --rm --name node -d -v %CD%:/src -w /src node:7.7.4-alpine node app.js
