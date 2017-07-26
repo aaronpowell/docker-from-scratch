97028c0a84475ee82879faf2f99f7ce814548083
Aaron Powell <me@aaron-powell.com>
2017-07-26 22:05:48 +1000

    adding port binding

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@ ECHO OFF
 
 ECHO nodejs image
 
-docker run -it --rm --name node -d -v %CD%:/src -w /src node:7.7.4-alpine node app.js
+docker run -it --rm --name node -d -v %CD%:/src -w /src -p 8080:3000 node:7.7.4-alpine node app.js
