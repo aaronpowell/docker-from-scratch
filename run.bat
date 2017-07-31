373cc84b0b0f1034ad45351ad9567cef26fbc560
Aaron Powell <me@aaron-powell.com>
2017-08-01 06:25:57 +1000

    Creating a dockerfile for our nodejs app

--- run.bat
+++ run.bat
@@ -2,4 +2,6 @@ ECHO OFF
 
 ECHO nodejs image
 
-docker run -it --rm --name node -d -v %CD%:/src -w /src -p 8080:3000 node:7.7.4-alpine node app.js
+docker build -t nodejs-app .
+
+docker run --rm -p 8080:3000 -d nodejs-app
