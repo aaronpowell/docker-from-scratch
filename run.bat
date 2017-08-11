827c26bd3d6d310435474a60e82ff200049a19d9
Aaron Powell <me@aaron-powell.com>
2017-08-11 14:30:33 +1000

    Creating a go app to run in scratch

--- run.bat
+++ run.bat
@@ -1,3 +1,15 @@
 ECHO OFF
 
-ECHO use VS 2017 to run the demo
+ECHO Building app
+
+docker run --rm -v %CD%/src:/src -w /src golang:1.8 go build -v -o app
+
+ECHO Creating image
+
+docker build -t dfs-scratch -f Dockerfile.run .
+
+ECHO Running container
+
+docker run --rm dfs-scratch
+
+docker rmi dfs-scratch
---
+++ src/app.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, everyone")
+    fmt.Println("I'm a Go app running from a scratch Docker container")
+}
