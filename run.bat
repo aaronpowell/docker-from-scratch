00ed1a9a0adb64643c7c40df1250aa8c83421102
Aaron Powell <me@aaron-powell.com>
2018-06-08 14:50:08 +1000

    rewrite to not use docker compose

--- run.bat
+++ run.bat
@@ -1,3 +1,29 @@
 ECHO OFF
 
-ECHO use VS 2017 to run the demo
+ECHO building the application
+
+cd src
+
+dotnet restore ./DemoApp.sln
+dotnet publish ./DemoApp.sln -c Release -o ./obj/Docker/publish
+
+ECHO creating the image
+
+docker build -t step11 ./DemoApp
+
+ECHO creating network
+
+docker network create step11
+
+ECHO starting containers
+docker run --rm -d --network step11 -p 1337:80 --name web -e ASPNETCORE_ENVIRONMENT=Development step11
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 --network step11 -d --name sql microsoft/mssql-server-linux
+
+PAUSE
+
+ECHO cleanup
+
+docker stop web
+docker stop sql
+docker rm sql
+docker network rm step11
