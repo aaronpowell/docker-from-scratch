0bbf823a41df14720542c2919722e8f9febc0347
Aaron Powell <me@aaron-powell.com>
2019-02-12 16:18:12 +1100

    updating the nodejs version

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@ ECHO OFF
 
 ECHO nodejs image
 
-docker run --platform=linux -it --rm --name node -d -v %CD%:/src -w /src node:7.7.4-alpine node app.js
+docker run --platform=linux -it --rm --name node -d -v %CD%:/src -w /src node:11.9.0-alpine node app.js
