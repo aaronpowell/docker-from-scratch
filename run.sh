c6cd1e246333d185c29508436c5d5799b3df0cee
Aaron Powell <me@aaron-powell.com>
2020-04-01 15:47:44 +1100

    had a broken binding

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo nodejs image
 
-docker run --platform=linux -it --rm --name node -d -v "$(pwd)/src" -w /src -p 8080:3000 node:7.7.4-alpine node app.js
+docker run --platform=linux -it --rm --name node -d -v "$(pwd):/src" -w /src -p 8080:3000 node:7.7.4-alpine node app.js
